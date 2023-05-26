ff590c6220dc68376e5392141389fab03e885f2b
Dominik K <dominik@koch-bautechnik.de>
2023-05-26 20:39:46 +0200

    fix: next build

---
+++ src/pages/imprint.tsx
@@ -0,0 +1,11 @@
+import dynamic from "next/dynamic";
+
+const Imprint = () => {
+  return (
+    <>
+      
+    </>
+
+  )
+}
+export default dynamic (() => Promise.resolve(Imprint), {ssr: false})
---
+++ src/pages/privacy.tsx
@@ -0,0 +1,11 @@
+import dynamic from "next/dynamic";
+
+const Privacy = () => {
+  return (
+    <>
+      
+    </>
+
+  )
+}
+export default dynamic (() => Promise.resolve(Privacy), {ssr: false})
