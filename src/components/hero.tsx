0d1337ed8dcd816bba75f985d9d5c78c73ee46e8
Dominik K <dominik@koch-bautechnik.de>
2023-05-28 00:30:55 +0200

    Fix: overflow

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
     <>
-      <div className='main z-[-999] bg-secondary'>
+      <div className='text-center main z-[-999] bg-secondary'>
      <div className='content' />
-     <h1 className="font-display  text-4xl font-extrabold leading-[1.15] text-white sm:text-6xl sm:leading-[1.15] ">
+     <h1 className="mt-10 font-display text-4xl font-extrabold leading-[1.15] text-white sm:text-6xl sm:leading-[1.15]">
      Empowering Through
         <br />
-        <span className="bg-gradient-to-r from-[#017ef0] via-[#00ace5] to-[#00ddd8] bg-clip-text text-transparent flex justify-center">
+        <span className="text-animation bg-clip-text text-transparent ">
         Innovation
         </span>
         </h1>
