db7b2aac53df872102364cdd8cb2dc2cf84d505d
Dibas Behera <46717953+beheradibas@users.noreply.github.com>
2023-10-15 18:03:00 +0530

    Added Contact Us button with Email

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,11 @@ export default function Home() {
         <p className="mt-6 text-2xl">
         Where Innovation Takes the Lead!
         </p>
-        
+        <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+          <a href="mailto:support@rivo.gg?subject=Contact" target="_blank" className="w-full px-5 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-primary rounded-md sm:mx-2 sm:order-2 sm:w-auto ">
+                  Contact Us
+          </a>
+        </div>
       </div>
     </main> </> 
   )
